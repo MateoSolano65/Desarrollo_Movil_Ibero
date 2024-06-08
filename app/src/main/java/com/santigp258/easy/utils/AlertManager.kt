eb3ba664a2403bc029647b31bf52e8e55498c566
santigp258 <santigp258@gmail.com>
2024-06-08 18:27:21 -0500

    fix: show snackbar on top of the keyboard if is open

--- app/src/main/java/com/santigp258/easy/utils/AlertManager.kt
+++ app/src/main/java/com/santigp258/easy/utils/AlertManager.kt
@@ -2,8 +2,6 @@ package com.santigp258.easy.utils
 
 import android.content.Context
 import android.view.View
-import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.santigp258.easy.R
@@ -12,24 +10,8 @@ import com.santigp258.easy.R
 class AlertManager(private val context: Context, private var view: View) {
 
     private fun showSnackBar(message: String, color: Int) {
-        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-
-        val snackbarView = snackbar.view
-        val params = snackbarView.layoutParams as ViewGroup.MarginLayoutParams
-
-        params.setMargins(params.leftMargin, 0, params.rightMargin, params.bottomMargin)
-        snackbarView.layoutParams = params
-
-        snackbarView.viewTreeObserver.addOnPreDrawListener(object :
-            ViewTreeObserver.OnPreDrawListener {
-            override fun onPreDraw(): Boolean {
-                snackbarView.viewTreeObserver.removeOnPreDrawListener(this)
-                snackbarView.translationY = 0f
-                return true
-            }
-        })
-
-        snackbar.setBackgroundTint(ContextCompat.getColor(context, color)).show()
+        Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+            .setBackgroundTint(ContextCompat.getColor(context, color)).show()
     }
 
     fun success(message: String) {
