b343603d29622ef0bfb27f3da07c72b366d921e7
santigp258 <santigp258@gmail.com>
2024-06-08 18:19:42 -0500

    chore: show snackbar at the top of the screen

--- app/src/main/java/com/santigp258/easy/utils/AlertManager.kt
+++ app/src/main/java/com/santigp258/easy/utils/AlertManager.kt
@@ -2,27 +2,45 @@ package com.santigp258.easy.utils
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.santigp258.easy.R
 
 
 class AlertManager(private val context: Context, private var view: View) {
+
+    private fun showSnackBar(message: String, color: Int) {
+        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+
+        val snackbarView = snackbar.view
+        val params = snackbarView.layoutParams as ViewGroup.MarginLayoutParams
+
+        params.setMargins(params.leftMargin, 0, params.rightMargin, params.bottomMargin)
+        snackbarView.layoutParams = params
+
+        snackbarView.viewTreeObserver.addOnPreDrawListener(object :
+            ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                snackbarView.viewTreeObserver.removeOnPreDrawListener(this)
+                snackbarView.translationY = 0f
+                return true
+            }
+        })
+
+        snackbar.setBackgroundTint(ContextCompat.getColor(context, color)).show()
+    }
+
     fun success(message: String) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-            .setBackgroundTint(ContextCompat.getColor(context, R.color.green))
-            .show()
+        showSnackBar(message, R.color.green)
     }
 
     fun error(message: String) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-            .setBackgroundTint(ContextCompat.getColor(context, R.color.red))
-            .show()
+        showSnackBar(message, R.color.red)
     }
 
     fun info(message: String) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-            .setBackgroundTint(ContextCompat.getColor(context, R.color.blue))
-            .show()
+        showSnackBar(message, R.color.blue)
     }
 }
